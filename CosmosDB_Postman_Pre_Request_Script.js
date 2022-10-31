4e577a2b3534983285a47e0c811c53afdc24e450
ashesmin <40337077+ashesmin@users.noreply.github.com>
2022-10-31 21:13:44 +0900

    Version 1.3

--- CosmosDB_Postman_Pre_Request_Script.js
+++ CosmosDB_Postman_Pre_Request_Script.js
@@ -1,92 +1,127 @@
-// store our master key for documentdb
-var mastKey = postman.getEnvironmentVariable("CosmosDBMasterKey");
-console.log("mastKey = " + mastKey);
+/**
+ * This POSTMAN pre-request script help to create necessary values
+ * to make a call to Cosmos DB REST API with Authorization request headers
+ * and set those headers to each request.
+ * See REST API into in: https://docs.microsoft.com/en-us/rest/api/cosmos-db/
+ * See access control reference on:
+ * https://docs.microsoft.com/en-us/rest/api/cosmos-db/access-control-on-cosmosdb-resources
+ * Josué Martínez Buenrrostro @josuemb on twitter
+ */
 
-// store our date as RFC1123 format for the request
-var today = new Date();
-var UTCstring = today.toUTCString();
-postman.setEnvironmentVariable("RFC1123time", UTCstring);
+// Checking for mandatory collection variables need to be present in any call, those are:
+// Authorization, x-ms-version, Authorization and x-ms-date.
+// When Authorization and x-ms-date values are going to be filled in this script.
+// See reference on: https://docs.microsoft.com/en-us/rest/api/cosmos-db/common-cosmosdb-rest-request-headers
+pm.test("Check for collectionVariables", function () {
+  let vars = [
+    "authorization_type",
+    "authorization_version",
+    "authorization_signature",
+    "x-ms-version",
+  ];
+  vars.forEach(function (item, index, array) {
+    pm.expect(
+      pm.collectionVariables.get(item),
+      item + " variable not set"
+    ).to.not.be.undefined;
+    pm.expect(
+      pm.collectionVariables.get(item),
+      item + " variable not set"
+    ).to.not.be.empty;
+  });
+});
 
-// Grab the request url
-var url = request.url.trim(); 
-console.log("request url = " + url);
+// Get URL value from request.
+let url = pm.request.url.toString();
+//Regular expression to separate parameters in pair with resource name and resource id when resource id is optional.
+let urlRegExp =
+  /^https?:\/\/.*\.documents\.azure\.com(?::\d+)?(?:\/([^\/]+)(?:\/([^\/]+)?)?)+$/i;
 
-// strip the url of the hostname up and leading slash
-var strippedurl = url.replace(new RegExp('^https?://[^/]+/'),'/');
-console.log ("stripped Url = " + strippedurl);
+// Checking for valid URL syntax.
+// See: https://docs.microsoft.com/en-us/rest/api/cosmos-db/cosmosdb-resource-uri-syntax-for-rest
+pm.test("Check for URL valid format", function () {
+  pm.expect(url, "URL not set").to.not.be.undefined;
+  pm.expect(url, "URL not set").to.not.be.empty;
+  pm.expect(
+    urlRegExp.test(url),
+    "URL does not have a valid format. See: https://docs.microsoft.com/en-us/rest/api/cosmos-db/cosmosdb-resource-uri-syntax-for-rest"
+  ).to.be.true;
+});
 
-// push the parts down into an array so we can determine if the call is on a specific item
-// or if it is on a resource (odd would mean a resource, even would mean an item)
-var strippedparts = strippedurl.split("/");
-var truestrippedcount = (strippedparts.length - 1);
-console.log(truestrippedcount);
+// Getting necessary values from collection variables.
+// See: https://learning.postman.com/docs/sending-requests/variables/
+let autorizationType = pm.collectionVariables.get("authorization_type");
+let autorizationVersion = pm.collectionVariables.get("authorization_version");
+let authorizationSignature = pm.collectionVariables.get(
+  "authorization_signature"
+);
+let cosmosDBApiVersion = pm.collectionVariables.get("x-ms-version");
 
-// define resourceId/Type now so we can assign based on the amount of levels
-var resourceId = "";
-var resType = "";
+// Decode authorization signature (it is originally base64 coded)
+let key = CryptoJS.enc.Base64.parse(authorizationSignature);
+// Set request date formatting as UTC
+let dateUtc = new Date().toUTCString();
+// Get request method (a.k.a. verb) to build text for authorization token
+let verb = pm.request.method;
 
-// its odd (resource request)
-if (truestrippedcount % 2)
-{
-    console.log("odd");
-    // assign resource type to the last part we found.
-    resType = strippedparts[truestrippedcount];
-    console.log(resType);
-    
-    if (truestrippedcount > 1)
-    {
-        // now pull out the resource id by searching for the last slash and substringing to it.
-        var lastPart = strippedurl.lastIndexOf("/");
-        resourceId = strippedurl.substring(1,lastPart);
-        console.log(resourceId);
-    }
+// Execute regular expression to extract some parts from URL.
+let parsedUrl = urlRegExp.exec(url);
+// Get resource type from URL
+let resourceType = parsedUrl[1];
+// Get resource ID from URL, if it is not present, we are getting undefined.
+let resourceId = parsedUrl[2];
+// Build regular expression to get all parameters from URL to build ResourceLink part.
+// See "Example Encoding" in: https://docs.microsoft.com/en-us/rest/api/cosmos-db/access-control-on-cosmosdb-resources
+let resourceLinkPattern =
+  /^https?:\/\/.*\.documents\.azure\.com(?::\d+)?\/(.*)$/i;
+let parsedResourceLink = resourceLinkPattern.exec(url);
+// Get resource LInke from expression and in case last character is / we just drop it out.
+let resourceLink =
+  parsedResourceLink[1].charAt(parsedResourceLink[1].length - 1) === "/"
+    ? parsedResourceLink[1].substring(
+        parsedResourceLink[1],
+        parsedResourceLink[1].length - 1
+      )
+    : parsedResourceLink[1];
+// When Resource Id is present in parameters, then Resource Link is whole parameters part,
+// elsewhere, then we need to drop last /resource type to
+// build Resource Link.
+// Example:
+// for URL: https://myaccount.documents.azure.com/dbs/MyCollection/colls/
+// Resource Link will be just: dbs/MyCollection
+if (resourceId === undefined) {
+  // Resource Id not provided
+  // We need to cut last part to left just Resource Id
+  resourceLink = resourceLink.substring(0, resourceLink.lastIndexOf("/"));
 }
-else // its even (item request on resource)
-{
-    console.log("even");
-    // assign resource type to the part before the last we found (last is resource id)
-    resType = strippedparts[truestrippedcount - 1];
-    console.log("resType");
-    // finally remove the leading slash which we used to find the resource if it was
-    // only one level deep.
-    strippedurl = strippedurl.substring(1);
-    console.log("strippedurl");
-    // assign our resourceId
-    resourceId = strippedurl;
-    console.log("resourceId");
-}
 
-// assign our verb
-var verb = request.method.toLowerCase();
+// Build string to be encrypted and used as signature.
+// See: https://docs.microsoft.com/en-us/rest/api/cosmos-db/access-control-on-cosmosdb-resources
+var text =
+  (verb || "").toLowerCase() +
+  "\n" +
+  (resourceType || "").toLowerCase() +
+  "\n" +
+  (resourceLink || "") +
+  "\n" +
+  dateUtc.toLowerCase() +
+  "\n\n";
+// Build key to authorize request.
+let signature = CryptoJS.HmacSHA256(text, key);
+// Code key as base64 to be sent.
+let signature_base64 = CryptoJS.enc.Base64.stringify(signature);
+// Build autorization token and encode it as URI to be sent.
+var authorizationToken = encodeURIComponent(
+  "type=" +
+    autorizationType +
+    "&ver=" +
+    autorizationVersion +
+    "&sig=" +
+    signature_base64
+);
 
-// assign our RFC 1123 date
-var date = UTCstring.toLowerCase();
-
-// parse our master key out as base64 encoding
-var key = CryptoJS.enc.Base64.parse(mastKey);
-console.log("key = " + key);
-
-// build up the request text for the signature so can sign it along with the key
-var text = (verb || "").toLowerCase() + "\n" + 
-               (resType || "").toLowerCase() + "\n" + 
-               (resourceId || "") + "\n" + 
-               (date || "").toLowerCase() + "\n" + 
-               "" + "\n";
-console.log("text = " + text);
-
-// create the signature from build up request text
-var signature = CryptoJS.HmacSHA256(text, key);
-console.log("sig = " + signature);
-
-// back to base 64 bits
-var base64Bits = CryptoJS.enc.Base64.stringify(signature);
-console.log("base64bits = " + base64Bits);
-
-// format our authentication token and URI encode it.
-var MasterToken = "master";
-var TokenVersion = "1.0";
-auth = encodeURIComponent("type=" + MasterToken + "&ver=" + TokenVersion + "&sig=" + base64Bits);
-console.log("auth = " + auth);
-
-// set our auth token enviornmental variable.
-postman.setEnvironmentVariable("authToken", auth);
+// Set request headers
+pm.request.headers.upsert({ key: "Accept", value: "application/json" });
+pm.request.headers.upsert({ key: "x-ms-version", value: cosmosDBApiVersion });
+pm.request.headers.upsert({ key: "x-ms-date", value: dateUtc });
+pm.request.headers.upsert({ key: "Authorization", value: authorizationToken });
